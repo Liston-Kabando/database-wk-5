f01144915845f901e74c5c00b576261a124f2909
Liston-Kabando <listonmacharia9@gmail.com>
2025-09-17 02:56:51 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,2 +1,6 @@
 # 1. Write an SQL query to drop an index named IdxPhone from customers table.
 DROP INDEX idxphone ON customers;
+# 2. Write an SQL query to create a user named bob with the password 'S$cu3r3!' , restricted to the localhost hostname.
+-- to create a user we use CREATE USER'...'@'host'IDENTIFIED BY 'pass'
+CREATE USER 'Bob'@'localhost'IDENTIFIED BY'S$cu3r3!';
+
